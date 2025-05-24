091e7d0de39150aad5511bf08e4a2d76da5edfc3
Marco Castillo Cabrera <marco.cast.cab@gmail.com>
2025-05-23 22:38:22 -0300

    modificacion index.js

--- index.js
+++ index.js
@@ -19,22 +19,60 @@ const server = createServer((req, res) => {
       // 1. Usar readFile para leer el archivo
       // 2. Establecer el Content-Type correcto
       // 3. Enviar el contenido al cliente
+      readFile(join(__dirname, 'views', 'home.html'), (err, data) => {
+        if (err) {
+          res.writeHead(500)
+          res.end('Error interno del servidor')
+          return
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(data)
+      })
     } else if (url === '/login') {
       // TODO: Servir el archivo login.html desde la carpeta views
       // 1. Usar readFile para leer el archivo
       // 2. Establecer el Content-Type correcto
       // 3. Enviar el contenido al cliente
+      readFile(join(__dirname, 'views', 'login.html'), (err, data) => {
+        if (err) {
+          res.writeHead(500)
+          res.end('Error interno del servidor')
+          return
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(data)
+      })
     } else if (url === '/register') {
       // TODO: Servir el archivo register.html desde la carpeta views
       // 1. Usar readFile para leer el archivo
       // 2. Establecer el Content-Type correcto
       // 3. Enviar el contenido al cliente
+      readFile(join(__dirname, 'views', 'register.html'), (err, data) => {
+        if (err) {
+          res.writeHead(500)
+          res.end('Error interno del servidor')
+          return
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(data)
+      })
     } else {
       // TODO: Servir archivos estáticos desde la carpeta public (imágenes y CSS)
       // 1. Usar join para construir la ruta del archivo
       // 2. Usar readFile para leer el archivo
       // 3. Establecer el Content-Type usando getContentType
       // 4. Enviar el contenido al cliente
+      const filePath = join(__dirname, 'public', url)
+      readFile(filePath, (err, data) => {
+        if (err) {
+          res.writeHead(404)
+          res.end('Archivo no encontrado')
+          return
+        }
+        const contentType = getContentType(url)
+        res.writeHead(200, { 'Content-Type': contentType })
+        res.end(data)
+      })
     }
   }
   // Manejo de solicitudes POST
@@ -44,6 +82,8 @@ const server = createServer((req, res) => {
       // 1. Establecer el código de estado 302
       // 2. Establecer el header Location: '/'
       // 3. Finalizar la respuesta
+      res.writeHead(302, { Location: '/' })
+      res.end() 
     } else {
       // Enviar respuesta 404 para rutas POST no válidas
       res.writeHead(404)
